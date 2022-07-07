8cd4816063eba56363e33ae74a12de67516bd537
Alexandru <T_alexandru87@yahoo.com>
2022-07-07 19:51:48 +0100

    completed the user and friends models

---
+++ src/db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS events_db;
+
+CREATE DATABASE events_db;
+
+USE events_db;
---
+++ src/models/friends.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+
+const connection = require("../config/connection");
+
+const User = require("./user");
+
+class Friends extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    foreignKey: {
+      references: User,
+      key: "id",
+    },
+  },
+
+  friends_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    foreignKey: {
+      references: User,
+      key: "id",
+    },
+  },
+
+  friends_id: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  modelName: "friends",
+};
+
+Friends.init(schema, options);
+
+module.exports = Friends;
