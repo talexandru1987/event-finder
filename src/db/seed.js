f2a2111f30ce85e5fe78de09a0441b2fbde3c5d2
Alexandru <T_alexandru87@yahoo.com>
2022-07-12 21:20:52 +0100

    completed adding seeds and seeds.js

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,39 +1,80 @@
 //TEMPLATE CODE
 
-// const connection = require("../config/connection");
-// const { User, Book } = require("../models");
-// const users = require("./users.json");
-// const books = require("./books.json");
+const connection = require("../config/connection");
+const { Events, Friends, Invites, Messages, UserEvents, User } = require("../models");
+const events = require("./events.json");
+const friends = require("./friends.json");
+const invites = require("./invites.json");
+const messages = require("./messages.json");
+const userEvents = require("./user_events");
+const users = require("./user.json");
 
-// const seedUsers = async () => {
-//   const promises = users.map((user) => User.create(user));
-//   await Promise.all(promises);
-//   console.log("Successfully seeded users");
-// };
+const seedUsers = async () => {
+  const promises = users.map((user) => User.create(user));
+  await Promise.all(promises);
+  console.log("Successfully seeded users");
+};
 
-// const seedBooks = async () => {
-//   await Book.bulkCreate(books);
-//   console.log("Successfully seeded books");
-// };
+const seedFriends = async () => {
+  const promises = friends.map((friend) => Friends.create(friend));
+  await Promise.all(promises);
+  console.log("Successfully seeded friends");
+};
 
-// const init = async () => {
-//   try {
-//     console.log("Seeding database...");
-//     await connection.sync({ force: true });
+const seedEvents = async () => {
+  const promises = events.map((event) => Events.create(event));
+  await Promise.all(promises);
+  console.log("Successfully seeded events");
+};
 
-//     // seed users
-//     await seedUsers();
+const seedUserEvents = async () => {
+  const promises = userEvents.map((userEvent) => UserEvents.create(userEvent));
+  await Promise.all(promises);
+  console.log("Successfully seeded user events");
+};
 
-//     // seed books
-//     await seedBooks();
+const seedInvites = async () => {
+  const promises = invites.map((invite) => Invites.create(invite));
+  await Promise.all(promises);
+  console.log("Successfully seeded invites");
+};
 
-//     console.log("Seeding complete!!");
-//   } catch (error) {
-//     console.log(`[ERROR]: Failed to seed | ${error.message}`);
-//   }
+const seedMessages = async () => {
+  const promises = messages.map((message) => Messages.create(message));
+  await Promise.all(promises);
+  console.log("Successfully seeded messages");
+};
 
-//   // kill node process
-//   process.exit(0);
-// };
+const init = async () => {
+  try {
+    console.log("Seeding database...");
+    await connection.sync({ force: true });
 
-// init();
+    // seed users
+    await seedUsers();
+
+    // seed friends
+    await seedFriends();
+
+    // seed events
+    await seedEvents();
+
+    // seed user events
+    await seedUserEvents();
+
+    // seed invites
+    await seedInvites();
+
+    // seed messages
+    await seedMessages();
+
+    console.log("Seeding complete!!");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to seed | ${error.message}`);
+  }
+
+  // kill node process
+  process.exit(0);
+};
+
+init();
