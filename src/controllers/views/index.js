8d9d90fbc635082f64f647e9571526210d6cfe15
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-07-18 17:34:23 +0100

    finished rendering api info into events cards

--- src/controllers/views/index.js
+++ src/controllers/views/index.js
@@ -28,55 +28,12 @@ const renderSearchEventsPage = async (req, res) => {
 
   const { data } = await axios.get(GOOGLE_EVENTS_URL, options);
 
-  console.log(data.events_results);
-  const events = data.events_results;
-
-  const eventCards = data.events_results.map((event) => {
-    return ``;
+  const events = data.events_results.map((event) => {
+    return { ...event, address: event.address.join(" ") };
   });
 
-  return res.render("searchEvents");
+  return res.render("searchEvents", { events });
 };
-
-const getInfoFromSearch = (data) =>{
-  if (data.events_results){
-    
-    const title = data.title;
-    const date= data?.albumOfTrack.date;
-    const when= data.when;
-    const address= data.address;
-    const thumbnail=data.thumbnail;
-
-    const eventCard = `<div class="event-card">
-    <div class="event-card-header">
-      <img
-        src="${thumbnail}"
-        alt="rover"
-      />
-    </div>
-    <div class="event-card-body">
-      <span class="tag tag-teal">Technology</span>
-      <h4>
-        ${title}
-      </h4>
-      <p>
-      ${address}
-      </p>
-      <div class="user">
-      <img
-        src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo"
-        alt="user"
-      />
-        <div class="user-info">
-          <h5>${date}</h5>
-          <small>${when}</small>
-        </div>
-      </div>
-    </div>
-  </div>`;
-
-  return eventCard;
-  };
 
 module.exports = {
   renderHomePage,
