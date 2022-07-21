6e171e1553f3b7e94e0dca241c0ee992a7b574ba
surajverma2587 <surajverma2587@gmail.com>
2022-07-21 21:47:48 +0100

    add ivite to DB

--- public/assets/js/friends.js
+++ public/assets/js/friends.js
@@ -1,48 +1,50 @@
 const myEventsContainer = $("#my-events-container");
 
+const friendModal = new bootstrap.Modal(
+  document.getElementById("friends-modal")
+);
+
 const handleInviteFriendModal = (event) => {
   const target = $(event.target);
 
   if (target.is('button[name="invite-friend-modal-btn"]')) {
     const eventId = target.attr("id");
 
-    const friendModal = new bootstrap.Modal(
-      document.getElementById("friends-modal")
-    );
-
     friendModal.show();
 
-    const inviteFriendBtn = $('button[name="invite-friend-btn"]');
+    const friendsContainer = $("#friends-container");
 
     const inviteFriendHandler = async (event) => {
       try {
         const target = $(event.target);
 
-        const friendId = target.attr("id");
+        if (target.is('button[name="invite-friend-btn"')) {
+          const friendId = target.attr("id");
 
-        const payload = { event_id: eventId, friend_id: friendId };
+          const payload = { event_id: eventId, friend_id: friendId };
 
-        const response = await fetch("/api/invites", {
-          method: "POST",
-          body: JSON.stringify(payload),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+          const response = await fetch("/api/invites", {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
 
-        const data = await response.json();
+          const data = await response.json();
 
-        if (data.success) {
-          friendModal.hide();
-        } else {
-          // TODO: handle error
+          if (data.success) {
+            window.location.assign("/my-events");
+          } else {
+            // TODO: handle error
+          }
         }
       } catch (error) {
         console.log(error.message);
       }
     };
 
-    inviteFriendBtn.click(inviteFriendHandler);
+    friendsContainer.on("click", inviteFriendHandler);
   }
 };
 
